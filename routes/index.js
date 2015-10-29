ce02310eb568af0a8d0cb08ea3744c06ae715461
4talesa <rondinellifortalesa@gmail.com>
2015-10-29 14:52:19 -0700

    Access-Control-Allow-Origin fixing

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 var app = require('../server');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res, next) {
 	
 	var operations = [];
