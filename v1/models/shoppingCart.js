21969922b02bb7814e1d0c831135d687dfafb944
4talesa <rondinellifortalesa@gmail.com>
2015-12-17 09:36:57 -0800

    shoppingcart fixing to insert default date

--- v1/models/shoppingCart.js
+++ v1/models/shoppingCart.js
@@ -30,7 +30,7 @@ exports.getAll = function (req, res, next) {
 exports.post = function (req, res, next) {
 	
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('insert into shoppingCart (id, idStore, idUser, status, idPaymentMethod, postDate, putDate, deleteDate) values ($1,$2,$3,$4,$5,now(),null,null) RETURNING _id, id, idStore, idUser, status, idPaymentMethod, postDate, putDate, deleteDate',[req.body.id,req.body.idStore,req.body.idUser,req.body.status,req.body.idPaymentMethod], function(err, result) {
+		client.query('insert into shoppingCart (id, idStore, idUser, status, idPaymentMethod, date, postDate, putDate, deleteDate) values ($1,$2,$3,$4,$5,now(),now(),null,null) RETURNING _id, id, idStore, idUser, status, idPaymentMethod, postDate, putDate, deleteDate',[req.body.id,req.body.idStore,req.body.idUser,req.body.status,req.body.idPaymentMethod], function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
@@ -130,7 +130,7 @@ exports.getByUser = function (req, res, next) {
 			}else{
 				if (result.rows.length == 0){
 					console.log('Not found, generate one.');
-					client.query('insert into shoppingCart (id, idStore, idUser, status, idPaymentMethod, postDate, putDate, deleteDate) values ($1,$2,$3,$4,$5,now(),null,null) RETURNING _id, id, idStore, idUser, status, idPaymentMethod, postDate, putDate, deleteDate',[req.params.idUser,req.params.idStore,req.params.idUser,'pending','1'], function(err, result) {
+					client.query('insert into shoppingCart (id, idStore, idUser, status, idPaymentMethod, date, postDate, putDate, deleteDate) values ($1,$2,$3,$4,$5,now(),now(),null,null) RETURNING _id, id, idStore, idUser, status, idPaymentMethod, postDate, putDate, deleteDate',[req.params.idUser,req.params.idStore,req.params.idUser,'pending','1'], function(err, result) {
 						done();
 						if (err) {
 							console.error(err);
