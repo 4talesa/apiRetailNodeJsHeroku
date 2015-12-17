c843e93dbf650840de78c85ef2af5a230126bea9
4talesa <rondinellifortalesa@gmail.com>
2015-12-16 16:44:54 -0800

    fixing shoppingCartItem.getByShoppingCart

--- v1/models/shoppingCartItem.js
+++ v1/models/shoppingCartItem.js
@@ -48,7 +48,7 @@ exports.post = function (req, res, next) {
 exports.getOne = function (req, res, next) {
 	
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query(default_select+' where p.id = $1',[req.params.id], function(err, result) {
+		client.query(default_select+' where pi.id = $1',[req.params.id], function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
@@ -122,7 +122,7 @@ exports.put = function (req, res, next) {
 exports.getByShoppingCart = function (req, res, next) {
 	
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query(default_select+' where p.idShoppingCart = $1',[req.params.id], function(err, result) {
+		client.query(default_select+' where pi.idShoppingCart = $1',[req.params.id], function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
